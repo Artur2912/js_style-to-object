262235c32954f4e09c7a3883057bea6fb38fe019
art <art@debian.art>
2025-09-18 19:26:45 +0300

    sol

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -7,6 +7,16 @@
  */
 function convertToObject(sourceString) {
   // write your code here
+  const obj = {};
+  const lines = sourceString.split(';').filter(line => line !== '');
+
+  for (const line of lines) {
+    const [key, value] = line.split(':');
+
+    obj[key.trim()] = value.trim();
+  }
+
+  return obj;
 }
 
 module.exports = convertToObject;
